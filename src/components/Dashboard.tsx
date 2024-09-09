abcbf10b15fb27c0fd371c3dc246459661e548e9
deradam <deradam1@gmail.com>
2024-09-09 23:44:16 +0200

    fix(button): small ui fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -60,7 +60,7 @@ const Dashboard: React.FC<DashboardProps> = ({ dashboardId }) => {
         />
         <button
           onClick={handleCreateTask}
-          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+          className="md:ml-2 md:mt-0 px-4 py-2 mt-2 w-full md:w-auto bg-blue-500 text-white rounded-md"
         >
           Add Task
         </button>
